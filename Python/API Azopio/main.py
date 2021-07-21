eac98d50fc36a418828080d49b160a63e046d95a
V̸̡͈̠̺͕̖͇͕͇̻̳͔̦̎̇̐̐̂ͅė̵̛̛͉̲͇̭̹͙́̑̈̋̉͛̆̄̍͘͝͝N̶̡̛̞̫͕̲̱̞̟̼̫͚͉̯̍́̄͐̉̾̈́́ͅỏ̵̜̮͚̯̫̝̬̖̩̾M̷̛̛̰͈̥̰̣͈͎̝͍̬͙̖͇͒̐̍̒͌̀͌̂̿̚͠͝ <87692048+Cantin-L@users.noreply.github.com>
2021-07-21 09:50:02 +0200

    Update main.py

--- Python/API Azopio/main.py
+++ Python/API Azopio/main.py
@@ -6,12 +6,6 @@ import __version__.py
 from openpyxl import load_workbook
 import smtplib 
 
-#donner le lien du fichier excel + ouverture de l'excel avec les différentes pages
-path=''
-wb = load_workbook(path)
-ws1 = wb['Factures Clients']
-ws2 = wb['Factures Fournisseurs']
-
 serveur = smtplib.SMTP('smtp.gmail.com', )    ## Connexion au serveur sortant (en précisant son nom et son port)
 serveur.starttls()    ## Spécification de la sécurisation
 serveur.login("", "")    ## Authentification
@@ -19,98 +13,121 @@ message = "Debut actualisation"    ## Message à envoyer
 serveur.sendmail("", "", message)    ## Envoie du message
 serveur.quit()    ## Déconnexion du serveur
 
-#incrementation de nos variables
+#déclaration du contenu du document, et du token  
+headers = {'Content-Type': 'application/json',
+           'Authorization': "Token ..."}
+
+#ouverture du fichier excel et des feuilles
+path=''
+wb = load_workbook(path)
+ws1 = wb['Factures Clients']
+ws2 = wb['Factures Fournisseurs']
+
+#incrémentation de nos variables
+page1 = page2 = 1
 o = i = 2
-n1 = n2 = 1
+n1=n2=0
 
-#lecture de la date / heure en direct
-a=datetime.datetime.now().timestamp()
+'''------------------------------------------------------------------------------------------------------------------'''
 
-#Bloc requete client
-url = "https://apifeed.sellsy.com/0/"
+#boucle pour les données clients
+url ="https://app.azopio.com/public_api/v1/documents/?date_start=2020-01-01&page=1&state_checked=checked&type=client"
+f = requests.get(url,headers=headers)
+data = f.content
+data_dict =json.loads(data)
 
-payload={'request': '1',
-'io_mode': 'json',
-'do_in': '{"method":"document.getList","params":{"doctype":"invoice","pagination":{"nbperpage":"5000"}}}'}
+for element in data_dict :
+    nombre_doc = data_dict["count"]
+    print (nombre_doc)
+    while n1!= nombre_doc :
+        url="https://app.azopio.com/public_api/v1/documents/?date_start=2020-01-01&page={0}&state_checked=checked&type=client".format(page1)
+        f = requests.get(url,headers=headers)
+        data = f.content
+        data_dict =json.loads(data)
+        page1+=1
+        print(url)
+        for element in data_dict["results"]:
+            uuid=element["uuid"]
+            print("Clients :",n1)       
+            url1 = "https://app.azopio.com/public_api/v1/document/{0}".format(uuid)
+            f1 = requests.get(url1,headers=headers)
+            data1 = f1.content
+            data_dict1 =json.loads(data1)
+            ws1['B'+str(o)] = data_dict1["issuer"]
+            ws1['C'+str(o)] = data_dict1["reference"]
+            ws1['D'+str(o)] = data_dict1["date"]
+            ws1['F'+str(o)] = data_dict1["currency"]
+            htt = data_dict1["untaxed_amount"].replace(",",".")
+            HT = float(htt)
+            ws1['G'+str(o)] = HT
+            ttct = data_dict1["total_amount"].replace(",",".")
+            TTC = float(ttct)
+            ws1['H'+str(o)] = TTC
+            tvat = data_dict1["vat_amount"].replace(",",".")
+            TVA = float(tvat)
+            ws1['I'+str(o)] = TVA
+            ws1['S'+str(o)] = data_dict1["state_paid"]
+            #ws2['T'+str(o)] = #data_dict3["inserted"]
+            ws1['V'+str(o)] = data_dict1["expense_category"]
+            ws1['W'+str(o)] = data_dict1["payment_method"]
+            ws1['X'+str(o)] = data_dict1["due_date"]
+            ws1['Y'+str(o)] = data_dict1["comment"]
+            ws1['Z'+str(o)] = data_dict1["web_url"]
+            o+=1
+            n1+=1
 
-headers = {
-  'Authorization': 'OAuth oauth_consumer_key="...",oauth_token="...",oauth_signature_method="PLAINTEXT",oauth_timestamp={0},oauth_nonce="...",oauth_version="1.0",oauth_signature="..."'.format(a)
-}
-res = requests.request("POST", url, headers=headers, data=payload)
-response=json.loads(res.text)
+'''--------------------------------------------------------------------------------------------------------------------'''
 
-#début de la boucle for pour remplissage d'Excel
-resp = response['response']['result']
-for valeur in resp :
-  print("Clients : n°",n1)
-  v=valeur
-  data=resp[v]
-  ws1['B'+str(i)] = data["thirdname"]
-  ws1['C'+str(i)] = data["id"]
-  ws1['D'+str(i)] = data["displayedDate"]
-  ws1['F'+str(i)] = data["currencysymbol"]
-  HT = float(data["totalAmountTaxesFree"])
-  ws1['G'+str(i)] = HT
-  TTC = float(data["totalAmount"])
-  ws1['H'+str(i)] = TTC
-  TVA = float(data["taxesAmountSum"])
-  ws1['I'+str(i)] = TVA
-  ws1['S'+str(i)] = data["step_label"]
-  ws1['T'+str(i)] = data["payDateText"]
-  ws1['T'+str(i)] = data["payDateCustom"]
-  ws1['V'+str(i)] = data["thirdRelationType"]
-  '''
-  ws1['W'+str(i)] = data["payment_method"]
-  '''
-  ws1['X'+str(i)] = data["formatted_payDateCustom"]
-  ''''
-  ws1['Y'+str(i)] = data["comment"]
-  ws1['Z'+str(i)] = data["web_url"]
-  '''
-  i+=1
-  n1+=1
+#boucle pour les données fournisseurs
+url2 ="https://app.azopio.com/public_api/v1/documents/?date_start=2020-01-01&page=1&state_checked=checked&type=supplier"
+f2 = requests.get(url2,headers=headers)
+data2 = f2.content
+data_dict2 =json.loads(data2)
 
-#Bloc requete fournisseur
-payload={'request': '1',
-'io_mode': 'json',
-'do_in': '{"method":"Purchase.getList","params":{"doctype":"invoice","pagination":{"nbperpage":"5000"}}}'}
+for element in data_dict2 :
+    nombre_doc = data_dict2["count"]
+    print (nombre_doc)
+    while n2!= nombre_doc :
+        url2="https://app.azopio.com/public_api/v1/documents/?date_start=2020-01-01&page={0}&state_checked=checked&type=supplier".format(page2)
+        f2 = requests.get(url2,headers=headers)
+        data2 = f2.content
+        data_dict2 =json.loads(data2)
+        page2+=1
+        print(url2)
+        for element1 in data_dict2["results"]:
+            uuid=element1["uuid"]
+            print("Fournisseurs :",n2)  
+            url3 = "https://app.azopio.com/public_api/v1/document/{0}".format(uuid)
+            f3 = requests.get(url3,headers=headers)
+            data3 = f3.content
+            data_dict3 =json.loads(data3)
+            if data_dict3["expense_category"]=='wages' :
+                n2+=1
+            else :
+                ws2['B'+str(i)] = data_dict3["issuer"]
+                ws2['C'+str(i)] = data_dict3["reference"]
+                ws2['D'+str(i)] = data_dict3["date"]
+                ws2['F'+str(i)] = data_dict3["currency"]
+                htt = data_dict3["untaxed_amount"].replace(",",".")
+                HT = float(htt)
+                ws2['G'+str(i)] = HT
+                ttct = data_dict3["total_amount"].replace(",",".")
+                TTC = float(ttct)
+                ws2['H'+str(i)] = TTC
+                tvat = data_dict3["vat_amount"].replace(",",".")
+                TVA = float(tvat)
+                ws2['I'+str(i)] = TVA
+                ws2['S'+str(i)] = data_dict3["state_paid"]
+                #ws2['T'+str(i)] = #data_dict3["inserted"]
+                ws2['V'+str(i)] = data_dict3["expense_category"]
+                ws2['W'+str(i)] = data_dict3["payment_method"]
+                ws2['X'+str(i)] = data_dict3["due_date"]
+                ws2['Y'+str(i)] = data_dict3["comment"]
+                ws2['Z'+str(i)] = data_dict3["web_url"]
+                i+=1 
+                n2+=1
 
-headers = {
-  'Authorization': 'OAuth oauth_consumer_key="",oauth_token="",oauth_signature_method="PLAINTEXT",oauth_timestamp={0},oauth_nonce="",oauth_version="1.0",oauth_signature=""'.format(a)
-}
-res = requests.request("POST", url, headers=headers, data=payload)
-response=json.loads(res.text)
-
-#début de la boucle for pour remplissage d'Excel
-resp = response['response']['result']
-for valeur in resp :
-  print("Fournisseurs : n°",n2)
-  v=valeur
-  data=resp[v]
-  ws2['B'+str(o)] = data["thirdname"]
-  ws2['C'+str(o)] = data["id"]
-  ws2['D'+str(o)] = data["displayedDate"]
-  ws2['F'+str(o)] = data["currencysymbol"]
-  HT = float(data["totalAmountTaxesFree"])
-  ws2['G'+str(o)] = HT
-  TTC = float(data["totalAmount"])
-  ws2['H'+str(o)] = TTC
-  TVA = float(data["taxesAmountSum"])
-  ws2['I'+str(o)] = TVA
-  ws2['S'+str(o)] = data["step_label"]
-  ws2['T'+str(o)] = data["payDateText"]
-  ws2['T'+str(o)] = data["payDateCustom"]
-  ws2['V'+str(o)] = data["thirdRelationType"]
-  '''
-  ws2['W'+str(i)] = data["payment_method"]
-  '''
-  ws2['X'+str(o)] = data["formatted_payDateCustom"]
-  '''
-  ws2['Y'+str(i)] = data["comment"]
-  ws2['Z'+str(i)] = data["web_url"]
-  '''
-  o+=1
-  n2+=1
+'''---------------------------------------------------------------------------------------------------------------'''
 
 serveur = smtplib.SMTP('smtp.gmail.com', )    ## Connexion au serveur sortant (en précisant son nom et son port)
 serveur.starttls()    ## Spécification de la sécurisation

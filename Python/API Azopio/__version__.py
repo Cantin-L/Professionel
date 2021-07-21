b8b4d27109e5248a2f57aee66057c5831409a01b
V̸̡͈̠̺͕̖͇͕͇̻̳͔̦̎̇̐̐̂ͅė̵̛̛͉̲͇̭̹͙́̑̈̋̉͛̆̄̍͘͝͝N̶̡̛̞̫͕̲̱̞̟̼̫͚͉̯̍́̄͐̉̾̈́́ͅỏ̵̜̮͚̯̫̝̬̖̩̾M̷̛̛̰͈̥̰̣͈͎̝͍̬͙̖͇͒̐̍̒͌̀͌̂̿̚͠͝ <87692048+Cantin-L@users.noreply.github.com>
2021-07-21 09:26:02 +0200

    Update __version__.py

--- Python/API Azopio/__version__.py
+++ Python/API Azopio/__version__.py
@@ -1,1 +1,9 @@
-print("Hello World")
+__title__ = 'Azopio'
+
+__description__ = 'https://app.azopio.com/public_api/v1/swagger-ui/ '
+
+__version__ = '0.0.4'
+
+__author__ = 'Cantin L.'
+
+print("API d' ", __title__ ," lien de la documentation : ", __description__ ," c'est la version ", __version__ ," développer par ", __author__)

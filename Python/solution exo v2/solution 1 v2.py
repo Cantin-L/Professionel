38aa6b0f5ae77724b1ae435feb20f2f2ccd7bb52
V̸̡͈̠̺͕̖͇͕͇̻̳͔̦̎̇̐̐̂ͅė̵̛̛͉̲͇̭̹͙́̑̈̋̉͛̆̄̍͘͝͝N̶̡̛̞̫͕̲̱̞̟̼̫͚͉̯̍́̄͐̉̾̈́́ͅỏ̵̜̮͚̯̫̝̬̖̩̾M̷̛̛̰͈̥̰̣͈͎̝͍̬͙̖͇͒̐̍̒͌̀͌̂̿̚͠͝ <87692048+Cantin-L@users.noreply.github.com>
2021-09-28 11:01:12 +0200

    Add files via upload

---
+++ Python/solution exo v2/solution 1 v2.py
@@ -0,0 +1,26 @@
+'''
+Auteur : Cantin L.
+Site web : https://itliocorp.fr
+Version : 0.2
+License : MIT 3.0
+
+Sujet : Afficher Hello World en python
+
+Notions : Variable, Types de variable, Afficher une variable, Fonction, Commentaires
+
+Fonction à utiliser :
+
+type(...)
+print(...)
+def ...():
+if (): 
+
+'''
+
+def main() :
+    a =("Hello World")
+    print(type(a))
+    print(a)
+
+if (__name__ == '__main__'):
+    main()

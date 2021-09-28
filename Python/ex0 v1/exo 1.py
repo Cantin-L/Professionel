96fcc7c24d8b664fb01b352b41b864d51a743233
V̸̡͈̠̺͕̖͇͕͇̻̳͔̦̎̇̐̐̂ͅė̵̛̛͉̲͇̭̹͙́̑̈̋̉͛̆̄̍͘͝͝N̶̡̛̞̫͕̲̱̞̟̼̫͚͉̯̍́̄͐̉̾̈́́ͅỏ̵̜̮͚̯̫̝̬̖̩̾M̷̛̛̰͈̥̰̣͈͎̝͍̬͙̖͇͒̐̍̒͌̀͌̂̿̚͠͝ <87692048+Cantin-L@users.noreply.github.com>
2021-09-28 10:52:48 +0200

    Add files via upload

---
+++ Python/ex0 v1/exo 1.py
@@ -0,0 +1,15 @@
+'''
+Auteur : Cantin L.
+Site web : https://itliocorp.fr
+Version : 0.1
+License : MIT 3.0
+
+Sujet : Afficher Hello World en python
+
+Notions : Afficher une variable, Commentaires
+
+Fonction à utiliser :
+
+print(...)
+
+'''
---
+++ Python/ex0 v1/exo 4.py
@@ -0,0 +1,32 @@
+'''
+Auteur : Cantin L.
+Site web : https://itliocorp.fr
+Version : 0.1
+License : MIT 3.0
+
+Sujet : Création d'une fonction
+1. Créer une fonction étoile mettre un paramétres "n";
+2. Faire une boucle conditionnel qui permet faire le nombre de ligne que tu veux;
+3. Afficher a chaque nouvelle ligne une étoile en plus.
+
+Résultat final à faire:
+
+*
+**
+***
+****
+*****
+******
+*******
+********
+*********
+**********
+
+Notions : Afficher une variable, Création d'une fonction, Boucle conditionnel, Commentaires
+
+Fonction à utiliser :
+def(...)
+print(...)
+for():
+
+'''
